185fa8e7b527debbd4a38df17243256469989dd0
Madhon <madhon@madhon.co.uk>
2023-12-13 16:31:31 +0000

    feat: .net 8.0 update

--- BuberDinner.Api/DependencyInjection.cs
+++ BuberDinner.Api/DependencyInjection.cs
@@ -2,6 +2,7 @@ namespace BuberDinner.Api;
 
 using BuberDinner.Api.Common.Errors;
 using BuberDinner.Api.Common.Mapping;
+using Microsoft.AspNetCore.HttpLogging;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,6 +11,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddPresentation(this IServiceCollection services)
     {
+        services.AddHttpLogging(o => o = new HttpLoggingOptions());
         services.AddControllers();
         services.AddMediator(opts => opts.ServiceLifetime = ServiceLifetime.Scoped);
         services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
